0f6dad9100f7d7f48bfefabe8f138914280bfa6d
olexey27 <allxyog@gmail.com>
2023-12-04 23:58:13 +0100

    base API controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,30 +3,29 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")] // /api/users
-public class UsersController : ControllerBase
+namespace API.Controllers
 {
+    public class UsersController : BaseApiController
+    {
     private readonly DataContext _context;
 
     public UsersController (DataContext context) 
-    {
+        {
         _context = context;
-    }
+        }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-    {
+        {
         var users = await _context.Users.ToListAsync();
 
         return users;
-    }
+        }
 
     [HttpGet("{id}")] // api/users/2
     public async Task<ActionResult<AppUser>> GetUser(int id)
-    {
+        {
         return await _context.Users.FindAsync(id);
+        }
     }
 }
--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,20 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController : BaseApiController
 {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController()
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
-
+        _logger = logger;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
