2e60397fe98d39b7ae3faaa4995f713b74753135
olexey27 <allxyog@gmail.com>
2023-12-10 21:38:19 +0100

    Anguler 17 auf 16 geander + npm und Node

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,18 +4,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
   title = 'WatchDate';
   users: any;
 
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
+    this.http.get('https://localhost:5001/api/users').subscribe ({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
     })
   }
+
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
