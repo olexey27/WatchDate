817bfb59a6bb0b4680713e6786c773ad68bfe37b
olexey27 <allxyog@gmail.com>
2024-01-22 22:08:30 +0100

    Cloudinary addet

---
+++ API/Helpers/CloudinarySettings.cs
@@ -0,0 +1,9 @@
+namespace API.Helpers
+{
+    public class CloudinarySettings
+    {
+        public int CloudName { get; set; }
+        public int ApiKey { get; set; }
+        public int ApiSecret { get; set; }
+    }
+}
