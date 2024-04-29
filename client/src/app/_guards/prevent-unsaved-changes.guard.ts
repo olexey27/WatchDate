b854c0cc2c60ac08d5e4a84b1b8efeca612bd658
olexey27 <allxyog@gmail.com>
2024-04-29 19:41:25 +0200

    confirmation service, tidying up the member mes

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,10 +1,14 @@
 import { CanDeactivateFn } from '@angular/router';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { inject } from '@angular/core';
+import { ConfirmService } from '../_services/confirm.service';
 
 export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
-if (component.editForm?.dirty) {
-  return confirm('Are you sure you want to continue? Any unsaved changes will be lost')
-}
+  const confirmService = inject(ConfirmService);
+
+  if (component.editForm?.dirty) {
+    return confirmService.confirm();
+  }
 
   return true;
 };
